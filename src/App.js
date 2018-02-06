d41df08afe1387a29f24245a4d8e5fdcd8c27a18
Raul Lopez Luna <raullopez2094@gmail.com>
2018-02-05 19:33:02 -0800

    searching for players updates page state and loads game data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ let queues = [];
 let champions = [];
 
 // TODO: use https://mushroom.teemo.gg/8.2/ for resources
+function errorCatching(response){
+  if(!response.ok){
+    throw Error(response.statusText)
+  }
+  return response;
+}
 
 const imgUrl = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/"//LeeSin_0.jpg
 // TODO:version might need to be updated per patch update
@@ -12,17 +18,7 @@ const iconUrl = "http://ddragon.leagueoflegends.com/cdn/8.2.1/img/profileicon/"/
 //gameID, then playerid
 const matchUrl = "https://matchhistory.na.leagueoflegends.com/en/#match-details/NA1/"//2704428580/216172135"
 
-class App extends Component {
-  render() {
-    return (
-      // <div className="App">
-        <MainWrapper></MainWrapper>
-        // {/* </div> */}
-    );
-  }
-}
-
-class MainWrapper extends Component{
+class App extends Component{
     constructor(props){
       super(props);
       this.state = {
@@ -31,9 +27,25 @@ class MainWrapper extends Component{
         queues:false,
         champions:false,
         sum0:"",
-        sum1:""
+        sum1:"",
+        region:'NA1',
       }
     }
+
+    regions = [
+      ["BR" , "BR1"],
+      ["EUNE", "EUN1"],
+      ["EUW", "EUW1"],
+      ["JP" ,"JP1"],
+      ["KR" ,"KR"],
+      ["LAN", "LA1"],
+      ["LAS", "LA2"],
+      ["NA" ,"NA1"],
+      ["OCE", "OC1"],
+      ["TR" ,"TR1"],
+      ["RU" ,"RU"],
+      ["PBE", "PBE1"],
+    ]
 
     componentDidMount(){
 
@@ -53,24 +65,59 @@ class MainWrapper extends Component{
         this.setState({champions: true})
       })
 
-      fetch('/demoData')
-      .then(response =>{ return response.json()})
+      // fetch('/demoData')
+      // .then(response =>{ return response.json()})
+      // .then(data => {
+      //   console.log("got match history data");
+      //   this.setState( {gameData : data, loaded:true})
+      //   // console.log(this.state.gameData);
+      // })
+    }
+
+    getPlayerInfo = (name, summoner) =>{
+      fetch(`/sumNameId/${this.state.region}/${name}`)
+      .then(response => {return response.json()})
       .then(data => {
-        console.log("got match history data");
-        this.setState( {gameData : data, loaded:true})
-        // console.log(this.state.gameData);
+        this.setState({[summoner]:data})
+        console.log(data);
       })
     }
 
+    getRegion = (e) =>{
+      this.setState({region: e.target.value})
+    }
+
+    // TODO: make sure both are valid summoners
+    showParentState = () =>{
+      console.log("showing updated State");
+      console.log(this.state.region);
+      if(this.state.sum0 && this.state.sum1){
+        console.log("both searches are good");
+        console.log(this.state.sum0)
+        console.log(this.state.sum1)
+        let url = `/findFriend/${this.state.region.toLowerCase()}/${this.state.sum0.accountId}/${this.state.sum1.accountId}`
+        console.log(url);
+        fetch(url)
+        .then(response =>{ return response.json()})
+        .then(data => {
+          console.log("got match history data");
+          console.log(data);
+          this.setState( {gameData : data, loaded:true}, console.log(this.state.gameData))
+        })
+      }
+    }
+
+    //get both summoner names from input fields
     parentSubmitHandler = (value, summoner) => {
-      console.log("got the info all the way up");
       console.log(`passed up value is "${value}"`);
 
       if(summoner ===0){
-        this.setState({sum0:value})
+        this.getPlayerInfo(value, "sum0");
+        // this.setState({sum0:value})
         console.log("sum0");
       }else{
-        this.setState({sum1:value})
+        // this.setState({sum1:value})
+        this.getPlayerInfo(value, "sum1");
         console.log("sum1");
       }
     }
@@ -101,7 +148,13 @@ class MainWrapper extends Component{
       return (
         <div className="App">
           <header className="App-header">
-            <SummonerSearch parentSubmitHandler={this.parentSubmitHandler}/>
+            <SummonerSearch
+              parentSubmitHandler={this.parentSubmitHandler}
+              regionHandler={this.getRegion}
+              selectedRegion={this.state.region}
+              regions={this.regions}
+            />
+            <button onClick={this.showParentState}>Show State</button>
           </header>
           {myElement}
         </div>
@@ -147,7 +200,6 @@ function Match (props){
 
         const findChamp = (champId) => {
           return champions.find(x => {
-            //TODO make sure this works
             return x.id === champId
           })
         }
@@ -158,29 +210,30 @@ function Match (props){
   return (
     <div className={"match " + checkWinColor()}>
 
-     <Player
-       lane={player1.lane}
-       champion={findChamp(player1.champId)}
-       stats={player1.stats}
-     />
-   <div className="matchInfo">
-      <p>{props.match.quickInfo.win === true ? "Victory" : "Defeat"}</p>
-    <p>Date: {matchDate(props.match.quickInfo.date)}</p>
-  <p>Length: {matchLength(props.match.quickInfo.length)} minutes</p>
-<button className="myButton"><a
-         target="_blank"
-         href={matchUrl+props.match.gameId+'/'+
-          props.match.participantIdentities[0].player.accountId}>Match Details</a></button>
-       <p>Queue: {getQueue(props.match.queueId)}</p>
+      <Player
+        lane={player1.lane}
+        champion={findChamp(player1.champId)}
+        stats={player1.stats}
+      />
+      <div className="matchInfo">
+        <p>{props.match.quickInfo.win === true ? "Victory" : "Defeat"}</p>
+        <p>Date: {matchDate(props.match.quickInfo.date)}</p>
+        <p>Length: {matchLength(props.match.quickInfo.length)} minutes</p>
+        <button className="myButton"><a
+          target="_blank"
+          href={matchUrl+props.match.gameId+'/'+
+          props.match.participantIdentities[0].player.accountId}>Match Details</a>
+        </button>
+        <p>Queue: {getQueue(props.match.queueId)}</p>
       </div>
-       <Player
-         lane={player2.lane}
-         // champ={props.match.champ2.name}
-         champion={findChamp(player2.champId)}
-         // img={props.match.champ2.key}
-         stats={player2.stats}
-       />
-  </div>
+      <Player
+        lane={player2.lane}
+        // champ={props.match.champ2.name}
+        champion={findChamp(player2.champId)}
+        // img={props.match.champ2.key}
+        stats={player2.stats}
+      />
+    </div>
   )
 }
 
@@ -221,7 +274,10 @@ class SearchBar extends Component{
     let re = /^[A-Za-z._0-9]+$/;
     if(re.test(str) ===true) {
       console.log("valid " + str);
-      this.setState({validated:"valid"})
+      this.setState({
+        validated: "valid",
+        formatted: str
+      })
     } else{
       console.log("invalid "+str);
       this.setState({validated:"invalid"})
@@ -231,28 +287,51 @@ class SearchBar extends Component{
   handleSubmit = (e) => {
     e.preventDefault(); //prevent redirect from form submission
     console.log("there was a form submit");
-    this.props.parentSubmitHandler(this.state.inputVal, this.props.summoner)
-
+    this.props.parentSubmitHandler(this.state.formatted, this.props.summoner)
   }
 
   render(){
     return(
       <form onSubmit={this.handleSubmit}>
         <input type ="text" value={this.state.inputVal} placeholder="Summoner Name" onChange={this.handleChange}></input>
-        <button disabled={this.state.validated != "valid"} type="submit">Search for Summoner</button>
+        <button disabled={this.state.validated !== "valid"} type="submit">Search for Summoner</button>
       </form>
     )
   }
 }
 
-function SummonerSearch(props){
-  return(
-    <div className="searchBarsDiv">
-      <p>Enter your in game summoner name, and a friend's</p>
-      <SearchBar parentSubmitHandler = {props.parentSubmitHandler} summoner={0}/>
-      <SearchBar parentSubmitHandler = {props.parentSubmitHandler} summoner={1}/>
-    </div>
-  )
+// TODO: make this component hold state for all input
+class SummonerSearch extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      sum1Name:"",
+      sum2Name:""
+    }
+  }
+
+
+
+  // handleChange = (e) => {
+  //   console.log("changing the value to ", e.target.value);
+  //   this.setState({region:e.target.value})
+  // }
+
+  render(){
+    return(
+      <div className="searchBarsDiv">
+        <p>Enter your in game summoner name, and a friend's</p>
+        <p>Region</p>
+        <select value={this.props.selectedRegion} onChange={this.props.regionHandler}>
+          {this.props.regions.map(region => {
+            return <option value={region[1]} key={region[0]}>{region[0]}</option>
+          })}
+        </select>
+        <SearchBar parentSubmitHandler = {this.props.parentSubmitHandler} summoner={0}/>
+        <SearchBar parentSubmitHandler = {this.props.parentSubmitHandler} summoner={1}/>
+      </div>
+    )
+  }
 }
 
 export default App;
